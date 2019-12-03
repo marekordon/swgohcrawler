723d2ad667f798a02d72e69a4fdb892ebf621797
marekordon <m.ordon@oberthur.com>
2019-12-03 15:27:48 +0100

    Working version

--- src/main/java/pl/ordon/marek/swgoh/crawler/Main.java
+++ src/main/java/pl/ordon/marek/swgoh/crawler/Main.java
@@ -13,7 +13,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) throws IOException {
         Document doc = Jsoup.connect("https://swgoh.gg/g/11228/perfekcyjna-niedoskonalosc/").get();
-        Elements elements = doc.select("body > div.container.p-t-md > div.content-container > div.content-container-primary.character-list > ul > li.media.list-group-item.p-0.b-t-0 > div > table > tbody > tr > td.footable-first-visible > a");
+        Elements elements = doc.select("body > div.container.p-t-md > div.content-container > div.content-container-primary.character-list > ul > li.media.list-group-item.p-0.b-t-0 > div > table > tbody > tr > td > a");
         Map<String,Integer> mapa = new HashMap<String, Integer>();
         for (Element element : elements) {
             ArrayList<String> chars = getCharactersByPleyer(element.attributes().get("href"));
@@ -29,8 +29,8 @@ public class Main {
     }
 
     private static ArrayList<String> getCharactersByPleyer(String address) throws IOException {
-        System.out.println(address + "characters/");
-        Document doc = Jsoup.connect(address + "characters/").get();
+        System.out.println("https://swgoh.gg" + address + "characters/");
+        Document doc = Jsoup.connect("https://swgoh.gg" + address + "characters/").get();
         Elements elements = doc.select("body > div.container.p-t-md > div.content-container > div.content-container-primary.character-list > ul > li.media.list-group-item.p-a.collection-char-list > div > div");
         ArrayList<String> characters = new ArrayList<String>();
         for(Element element: elements){
